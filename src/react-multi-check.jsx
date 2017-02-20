d6dffcfe9209e0353d02b0122d67ab38cee6156d
Manoj Singh Negi <justanothermanoj@gmail.com>
2017-02-20 16:55:49 +0530

    added elements and onclick

--- src/react-multi-check.jsx
+++ src/react-multi-check.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
 
+const Checkable = ({label, onSelect, selected, onClick}) => {
+	return (
+		<div onClick={onClick && onClick} className={"checkable" + ( selected ? ' active' : '')}>
+			<span className="label">{label}</span>
+			{selected ? onSelect : null}
+		</div>
+	)
+}
+
 export default class MultiCheck extends React.Component {
+
+	onElemSelect = (index) => {
+		console.log(index)
+	}
+
 	render() {
+		let {elements} = this.props;
 		return (
-			<div>
-				Multi Check
+			<div className="react-multi-check">
+				{elements ? elements.map((element, index) => {
+					return <Checkable label={element.label} onSelect={"selected"} onClick={() => console.log(index)} selected={true} />
+				}) : null}
 			</div>
 		)
 	}
